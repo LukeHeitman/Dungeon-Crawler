11935a5dc95a93b7bc64a229b6ebc7ec7cf4801e
Jackson <Jackson@Jacksons-Macbook-Pro-6.local>
2018-12-03 18:58:38 -0500

    ghost

--- template.py
+++ template.py
@@ -47,8 +47,8 @@ def main():
         DISPLAYSURFACE.fill(BLACK)
         DISPLAYSURFACE.blit(IMAGEDICT['player'], (playerX,playerY))
         DISPLAYSURFACE.blit(IMAGEDICT['bronzekey'], (10,10))
-        DISPLAYSURFACE.blit(IMAGEDICT['silverkey'], (50,50))
-        DISPLAYSURFACE.blit(IMAGEDICT['goldkey'], (100,100))
+        #DISPLAYSURFACE.blit(IMAGEDICT['silverkey'], (50,50))
+        #DISPLAYSURFACE.blit(IMAGEDICT['goldkey'], (100,100))
 
         for event in pygame.event.get():
             if event.type == QUIT:
