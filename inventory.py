10f524f41a7086147e48c2f0b4820f6ac1c724c6
Jackson <Jackson@Jacksons-Macbook-Pro-6.local>
2018-12-06 17:22:37 -0500

    deleted inventory

--- inventory.py
+++
@@ -1,8 +0,0 @@
-
-# creates a player inventory that keys will be added to
-playerinv []
-if :
-    if len(inv) < 6:
-        playerinv.append("somekey")              
-    else:
-        print("No inventory space")
